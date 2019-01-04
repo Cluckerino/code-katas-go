bf73ad7e1e2a818546c91808896f5a3d3231e055
Cluckerino <elitedragonslayer@gmail.com>
2019-01-03 16:17:39 -0800

    Stub and tests for kata

---
+++ primesteps/steps_in_primes.go
@@ -0,0 +1,14 @@
+package primesteps
+
+// Given the bounds, find the smallest pair of primes that differ by the given gap/step.
+// See: https://www.codewars.com/kata/steps-in-primes/go
+
+// Step finds the smallest pair of prime (p, q) where p, q in [m, n],
+// q > p, and q - p = g
+// g: gap between the two primes
+// m: inclusive lower limit of prime search
+// n: inclusive upper limit of prime search
+// returns: p, q in a slice, if found; otherwise, nil
+func Step(g, m, n int) []int {
+	return nil
+}
